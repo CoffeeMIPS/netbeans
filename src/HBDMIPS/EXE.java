47bdb9f149de5ea7b338573ceb0423cb24fe6809
Leo <Leo@Aidin>
2015-05-22 22:51:02 +0430

    Document CU, Register_file; EXE in Progress.

--- src/HBDMIPS/EXE.java
+++ src/HBDMIPS/EXE.java
@@ -7,21 +7,42 @@ package HBDMIPS;
  * @author HBD
  */
 public class EXE {
-	private ID_EXE idexe = new ID_EXE();
-	private EXE_MEM exemem = new EXE_MEM();
-        public String prev_instruction = "";
-        private String j_pc;
+	private ID_EXE idexe = new ID_EXE();// ID/EXE for EXE stage.
+	private EXE_MEM exemem = new EXE_MEM();// EXE/MEM for EXE stage.
+        public String prev_instruction = "";// ?????????????????????????????????
+        private String j_pc;//If Instruction is jump PC should change.
 	public EXE(ID_EXE idexe, EXE_MEM exemem) {
 		this.exemem = exemem;
 		this.idexe = idexe;
 	}
         
+        
+        
+        /**
+         * Is it Jump Register Instruction?
+         * Detect by functionBit which is contained in 6 low value bits
+         * of SignExtend.
+         * 
+         * @return isJReg - Boolean representing detection of JR. 
+         */
         public boolean isJumpReg(){
             String func_bit = getIdexe().getSignExt().substring(26, 32);
             if("001000".equals(func_bit))
                 return Boolean.TRUE;
             return Boolean.FALSE;
         }
+        
+        
+        /**
+         * Is it System Call Register Instruction?
+         * 
+         * Detect by functionBit which is contained in 6 low value bits
+         * of SignExtend of ID/EXE
+         * and also ALUOp which is contained in ControlBits of 
+         * ID/EXE in substrings [1,2] , [11,12].
+         * Note : In our convention we use 2 of 2bits ALUOp.
+         * @return isSysCall - Boolean representing detection of SysCall.
+         */
         public boolean isSyscall(){
             String func_bit = getIdexe().getSignExt().substring(26, 32);
             String ALUOp = getIdexe().getControlBits().substring(1, 3)+getIdexe().getControlBits().substring(11, 13);
@@ -30,33 +51,77 @@ public class EXE {
             }
             return false;
         }
+        
+        
+        /**
+         * Detects If Instruction contains value which
+         * should be written back IN RegisterFile? [LW..]
+         * It uses ID/EXE Control Bits to detect.[bit 1]
+         * @return isRW - Boolean representing detection of RegisterWrite.
+         */
         public boolean isRegwrite(){
             return idexe.getControlBits().charAt(0)=='1';
         }
+        
+        
+        /**
+         * Detects if Instruction is Jump ??????????????????????????????????????
+         * It uses ID/EXE Control Bits to detect.[bit 11]
+         * @return isJ - Boolean representing detection of Jump. 
+         */
         public boolean isJump(){
             return idexe.getControlBits().charAt(10)=='1';
         }
+        
+        
+        
+        /**
+         * Detects if Instruction is Branch Equal?[beq]
+         * It uses ID/EXE Control Bits to detect.[bit 7]
+         * @return 
+         */
         public boolean isBranch(){
             return idexe.getControlBits().charAt(6)=='1';
         }
+        
+        
+        /**
+         * Detects if Instruction is Branch not Equal[bnq].
+         * It uses ID/EXE Control Bits to detect.[bit 10]
+         * @return isBnq - Boolean representing if its branch not equal.
+         */
         public boolean isNot(){
             return idexe.getControlBits().charAt(9)=='1';
         }
+        
+        
+        /**
+         * Take a look at implementation :D
+         * Explained there.
+         */
 	public void action() {
                 
 		boolean REG_DEST = (getIdexe().getControlBits().charAt(7)) == '0' ? false : true;
+                                            //false means RT Should be used,  an I-Type instruction.
+                                            //true means RD Should be used, is a R-Type instruction. 
 		String ALUOp = getIdexe().getControlBits().substring(1, 3)+getIdexe().getControlBits().substring(11, 13);
-		boolean ALU_Src = (getIdexe().getControlBits().charAt(3)) == '0' ? false
-				: true;
-		String func_bit = getIdexe().getSignExt().substring(26, 32);
+                                            //which is used in ALUControl
+                boolean ALU_Src = (getIdexe().getControlBits().charAt(3)) == '0' ? false
+				: true;// false means use readData is an R-type instruction.
+                                        //true means use signExtend . an I-Type instruction.
+		String func_bit = getIdexe().getSignExt().substring(26, 32);//use SignExtend to 
+                                                                            //detect function bits.
 		if (REG_DEST) {
-			exemem.setWrite_Register(getIdexe().RD);
-		} else if (!REG_DEST) {
+			exemem.setWrite_Register(getIdexe().RD);//RD Should be used.
+                        //is a R-Type.
+		} else if (!REG_DEST) {//RT should be used.Is a I-Type.
 			exemem.setWrite_Register(getIdexe().RT);
 		}
-                String alucu_func = alu_cu(func_bit,ALUOp);
+                String alucu_func = alu_cu(func_bit,ALUOp);//initialize ALUControl.
                 if(isJump()){
-                    setJ_pc(getIdexe().getSignExt().substring(6,32));
+                    setJ_pc(getIdexe().getSignExt().substring(6,32));//PC changes
+                                                           //to the signExtend 
+                                                            //substring []
                 }
                 if(alucu_func == "1110" || alucu_func == "1111"){
                     int data1 = getIdexe().getRT_DATA();
