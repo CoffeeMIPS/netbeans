7f505617ad0820973d182cf14f0d3120a829593e
Leo <Leo@Aidin>
2015-05-23 09:28:48 +0430

    Document EXE/MEM.

--- src/HBDMIPS/MEM.java
+++ src/HBDMIPS/MEM.java
@@ -2,12 +2,27 @@ package HBDMIPS;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Represents Memory Stage.
+ * @author HBD
+ */
 public class MEM {
 
+        /**
+         * Stored as a List of String Represents Memory.
+         */
 	List<String> data_mem;
+        /**
+         * EXE/MEM Pipeline Register of MEM stage.
+         */
 	EXE_MEM exemem;
+        /**
+         * MEM/WB Pipeline Register of MEM stage.
+         */
 	MEM_WB memwb;
+        /**
+         * ???????????????????????????????????????????????????????????????????
+         */
 	IF stage_if;
 
 	public MEM(EXE_MEM exemem, MEM_WB memwb, IF stage_If) {
@@ -16,7 +31,20 @@ public class MEM {
 		this.stage_if = stage_If;
 		data_mem = new ArrayList<>(FileHandler.FileIO.FiletoStringArray("dataCache.txt"));
 	}
-
+        /**
+         * Do the job of Memory.
+         * This includes:
+         * 1- Get controlBits from EXE/MEM Pipeline Register.
+         * 2- If branch taken, then Set PC of the instruction to 
+         *     branch's pc.
+         * 3- If MEM_Write is Set then we should write Data which 
+         *    is stored in RT_DATA in EXE/MEM, to the address of 
+         *    memory that ALU_Result points to.
+         * 4- If MEM_READ is Set then we should read Data which
+         *    ALU_Result points to its address in memory.
+         * 5- Store ALU_Result, WriteRegister, controlBits in 
+         *    in MEM/WB Pipeline Register.
+         */
 	public void action() {
 		boolean MEM_READ = (exemem.getControlBits().charAt(4)) == '0' ? false
 				: true;
@@ -43,11 +71,15 @@ public class MEM {
 	}
         
         
-        	public String print() {
-                    String print="";
-		for (int i = 0; i < data_mem.size(); i++) {
-                    print+="Cell  [" + i + "] : " + Integer.parseInt(data_mem.get(i)) +"\n";
-		}
-                return print;
-	}
+        /**
+         * View current Memory.
+         * @return Print - memory in a String each cell in a line formatted. 
+         */        
+        public String print() {
+            String print="";
+            for (int i = 0; i < data_mem.size(); i++) {
+                print+="Cell  [" + i + "] : " + Integer.parseInt(data_mem.get(i)) +"\n";
+            }
+            return print;
+        }
 }
